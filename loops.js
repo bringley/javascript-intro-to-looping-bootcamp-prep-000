280b0378ade050964a1283906ca95d405efccb5f
Brian Ringley <brian.ringley@wework.com>
2018-01-28 21:29:32 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -18,4 +18,11 @@ function whileLoop(n) {
 
 function doWhileLoop(array) {
   
+  function maybeTrue() {
+    return Math.random() >= 0.5
+  }
+  
+  do {
+    array.pop()
+  } while (maybeTrue() && array.length > 0)
 }
